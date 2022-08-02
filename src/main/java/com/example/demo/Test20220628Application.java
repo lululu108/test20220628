ddad2c28f125ed4167c141149e39cdb8753e0a6d
DESKTOP-61RINEG\ユーザー <nemukoo@gmail.com>
2022-08-02 09:48:23 +0900

    commit01

---
+++ src/main/java/com/example/demo/Test20220628Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test20220628Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test20220628Application.class, args);
+	}
+
+}
